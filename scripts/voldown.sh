20db1bb736e531418b2d6113b745fe19af9182fb
Smolegit <smolegit@protonmail.com>
2023-08-22 00:50:33 -0400

    Removed useless scripts

--- scripts/voldown.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash 
-amixer -q -D pulse sset Master 4%-
-sleep 0.01
-vol=$(awk -F"[][]" '/Mono:/ { print $2 }' <(amixer sget Master))
-notify-send -i volume-1 -c volume -h int:value:$vol -h string:hlcolor:#fff "Volume Down :D" $vol 
--- scripts/volup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash 
-amixer -q -D pulse sset Master 4%+
-sleep 0.01
-vol=$(awk -F"[][]" '/Mono:/ { print $2 }' <(amixer sget Master))
-notify-send -i volume-2 -c volume -h int:value:$vol -h string:hlcolor:#ffffff "Volume Up :D" $vol 
-
-
