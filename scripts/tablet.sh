e1836d6d2672d3e1c45866dd29397e72e4f82a0f
Smolegit <smolegit@protonmail.com>
2023-10-04 11:14:51 -0400

    Update kitty and tablet script

--- scripts/tablet.sh
+++ scripts/tablet.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-xsetwacom set "Wacom Intuos PT S 2 Pen stylus" MapToOutput HDMI-A-0
+xsetwacom set "Wacom Intuos PT S 2 Pen stylus" MapToOutput DisplayPort-0
 #xsetwacom set "Wacom Intuos PT S 2 Pen stylus" Rotate half
